d3360ebdd86becdcb36b4ff282cf40507e6225e5
bltnico <bellot.n@gmail.com>
2019-02-22 18:09:47 +0100

    Add props demo

--- src/demo-components.js
+++ src/demo-components.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import SimpleComponentClass from './creer-un-composant/class/Component';
 import SimpleClassMethod from './creer-un-composant/class/Method';
 import SimpleStateDemo from './creer-un-composant/state/State';
 import SimpleControlledComponent from './creer-un-composant/state/ControlledComponent';
 import ComponentUpdate from './creer-un-composant/class/ComponentUpdate';
 import ApplicationFeed from './creer-un-composant/application/Feed';
+import SimplePropsDemo from './creer-un-composant/props/Props';
 
 const components = [
   {
@@ -30,6 +32,10 @@ const components = [
   {
     hash: 'creer-un-composant/application-feed',
     render: () => <ApplicationFeed />,
+  },
+  {
+    hash: 'creer-un-composant/props',
+    render: () => <SimplePropsDemo />,
   },
 ];
 
