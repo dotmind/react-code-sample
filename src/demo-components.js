8c2878adcd9580eac30931f197a4a202ae599c06
bltnico <bellot.n@gmail.com>
2019-02-22 17:42:36 +0100

    Update

--- src/demo-components.js
+++ src/demo-components.js
@@ -2,7 +2,9 @@ import React from 'react';
 import SimpleComponentClass from './creer-un-composant/class/Component';
 import SimpleClassMethod from './creer-un-composant/class/Method';
 import SimpleStateDemo from './creer-un-composant/state/State';
+import SimpleControlledComponent from './creer-un-composant/state/ControlledComponent';
 import ComponentUpdate from './creer-un-composant/class/ComponentUpdate';
+import ApplicationFeed from './creer-un-composant/application/Feed';
 
 const components = [
   {
@@ -18,8 +20,16 @@ const components = [
     render: () => <SimpleStateDemo />,
   },
   {
+    hash: 'creer-un-composant/controlled-component',
+    render: () => <SimpleControlledComponent />,
+  },
+  {
     hash: 'creer-un-composant/component-update',
     render: () => <ComponentUpdate />,
+  },
+  {
+    hash: 'creer-un-composant/application-feed',
+    render: () => <ApplicationFeed />,
   },
 ];
 
