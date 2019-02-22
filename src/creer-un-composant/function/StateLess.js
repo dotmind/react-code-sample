a0b74ff0aee99186f3a47bd6b75f7a5bf50ebb8f
bltnico <bellot.n@gmail.com>
2019-02-22 18:16:30 +0100

    Update stateless example

--- src/creer-un-composant/function/StateLess.js
+++ src/creer-un-composant/function/StateLess.js
@@ -2,4 +2,9 @@ import React from 'react';
 
 const Sample = () => <p>StateLess sample</p>;
 
-export default Sample;
+const SampleWithProps = (props) => <p>StateLess with props: {props.text}</p>;
+
+export default {
+  Sample,
+  SampleWithProps,
+};
